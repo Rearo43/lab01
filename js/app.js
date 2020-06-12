53b7c5c75f2cbe9134c9be7cce4709e1b3f255cf
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-06-11 23:49:03 -0700

    finalized color set

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('up and working');
+// let color = rgb[(36, 160, 11)]
+// let i = 
+// if (let i = 0; i < color.length; i++)
+
+let px = $(window).width() > 786
+
+function changeCSS(px){
+    if(px < 786){
+        $("main:nth-child(1)").css("background-color", "black");
+    }else(px > 786);{
+        $("main:nth-child(1)").css("background-color", "yellow");
+    }
+    changeCSS();
+}
